9b95614bc2774f9668dc6e3038519ad3e947eb30
Zi <jamesli3225@gmail.com>
2016-10-16 22:28:42 -0700

    add quiz-2

--- src/main/java/csula/cs4660/quizes/App.java
+++ src/main/java/csula/cs4660/quizes/App.java
@@ -12,15 +12,18 @@ public class App {
         // to get a state, you can simply call `Client.getState with the id`
         State initialState = Client.getState("10a5461773e8fd60940a56d2e9ef7bf4").get();
         System.out.println(initialState);
+        State endState = Client.getState("e577aa79473673f6158cc73e0e5dc122").get();
         // to get an edge between state to its neighbor, you can call stateTransition
         System.out.println(Client.stateTransition(initialState.getId(), initialState.getNeighbors()[0].getId()));
 
         Queue<State> frontier = new LinkedList<>();
         Set<State> exploredSet = new HashSet<>();
         Map<State, State> parents = new HashMap<>();
+        List<String> result = new LinkedList<>();
         frontier.add(initialState);
 
-        while (!frontier.isEmpty()) {
+        Boolean endTile = false;
+        while (endTile != true) {
             State current = frontier.poll();
             exploredSet.add(current);
 
@@ -29,27 +32,36 @@ public class App {
                 // state transition
                 if (neighbor.getId().equals("e577aa79473673f6158cc73e0e5dc122")) {
                     // construct actions from endTile
-                    System.out.println("found solution with depth of " + findDepth(parents, current, initialState));
+                    //System.out.println("found solution with depth of " + findDepth(parents, current, initialState));
+                    //System.out.println("ends");
+                    endTile = true;
                 }
                 if (!exploredSet.contains(neighbor)) {
                     parents.put(neighbor, current);
                     frontier.add(neighbor);
+                    //System.out.println("println");
                 }
             }
         }
 
-        System.out.println("Not found solution");
+        System.out.println("BFS Path: ");
+        printPath(parents, initialState, endState);
     }
 
-    public static int findDepth(Map<State, State> parents, State current, State start) {
-        State c = current;
-        int depth = 0;
+    public static void printPath(Map<State, State> parents, State initialState, State endState){
+        List<String> result = new LinkedList<>();
 
-        while (!c.equals(start)) {
-            depth ++;
-            c = parents.get(c);
+        while(!initialState.equals(endState)){
+            result.add(parents.get(endState).getLocation().getName()+ " : " + endState.getLocation().getName()
+                    + " : " + Client.stateTransition(parents.get(endState).getId() ,
+                    endState.getId()).get().getEvent().getEffect());
+            endState = parents.get(endState);
         }
 
-        return depth;
+        Collections.reverse(result);
+
+        for(String print : result){
+            System.out.println(print);
+        }
     }
 }
