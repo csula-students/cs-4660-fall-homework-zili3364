9faed842b4c4d56e7327beacf443a1cba38dd4c7
Zi <jamesli3225@gmail.com>
2016-12-03 06:13:51 -0800

    tron

---
+++ src/main/java/csula/cs4660/project/player.java
@@ -0,0 +1,68 @@
+package csula.cs4660.project;
+
+/**
+ * Created by James on 12/3/2016.
+ */
+import java.util.*;
+        import java.io.*;
+        import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        int x = 0;
+        int y = 0;
+        String move;
+
+        int[][] board = new int [20][30];
+
+        for (int i = 0; i < 20; i++){
+            for(int j = 0; j < 30; j++){
+                System.out.print(board[i][j]);
+            }
+        }
+
+        move = "RIGHT";
+        // game loop
+        while (true) {
+            int N = in.nextInt(); // total number of players (2 to 4).
+            int P = in.nextInt(); // your player number (0 to 3).
+            for (int i = 0; i < N; i++) {
+                int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
+                int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
+                int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
+                int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+                if(P == i){
+                    x = X1;
+                    y = Y1;
+                    System.err.println("X1: " + X1 + " Y1: " + Y1);
+                }
+                board[X1][Y1] = 1;
+
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+            move = nextMove(board, x, y);
+            System.out.println(move); // A single line with UP, DOWN, LEFT or RIGHT
+        }
+    }
+
+    public static String nextMove(int[][] board, int x, int y){
+        if(x == 0 || x == 29){
+            return "UP";
+        }
+        else if(y == 0 || y == 19){
+            return "LEFT";
+        }
+
+        return "RIGHT";
+    }
+
+}
